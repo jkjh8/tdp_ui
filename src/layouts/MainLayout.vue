926fedefc11a81d5f5aa2e42f95b94e3051fe507
jkjh8 <jkjh82@naver.com>
2023-07-16 01:05:24 +0900

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,78 @@
+<script setup>
+import { onBeforeMount } from 'vue'
+import { useRouter } from 'vue-router'
+// import { socket } from 'src/boot/socketio'
+// import {
+//   playerState,
+//   playerTimes,
+//   isFullscreen,
+//   devices,
+//   device
+// } from 'src/composables/usePlayerState'
+
+// import HeaderMenu from 'src/components/layout/headerMenus.vue'
+// import HeaderMenuSmall from 'src/components/layout/headerMenuSmall'
+// // import UserStatus from "components/layout/headerUserStatus.vue";
+// import PlayerControls from 'components/controls/playerControls.vue'
+const router = useRouter()
+onBeforeMount(() => {
+  console.log(window.location.href)
+})
+// onBeforeMount(() => {
+//   socket.on('connect', () => {
+//     console.log(`connecting to socket.io id=${socket.id}`)
+//   })
+
+//   socket.on('playerstate', (args) => {
+//     playerState.value = { ...args }
+//   })
+//   socket.on('status', (args) => {
+//     playerState.value = { ...args }
+//   })
+//   socket.on('times', (args) => {
+//     playerTimes.value = { ...args }
+//   })
+//   socket.on('devices', (args) => {
+//     devices.value = JSON.parse(args).filter((d) => d.kind == 'audiooutput')
+//   })
+//   socket.on('device', (args) => {
+//     device.value = args.device ? args.device : 'default'
+//   })
+//   socket.on('fullscreen', (args) => {
+//     isFullscreen.value = args.fullscreen
+//   })
+// })
+</script>
+
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="header row justify-between items-center">
+      <div class="row q-gutter-x-sm pointer" @click="router.push('/')">
+        <q-icon name="svguse:icons.svg#logo" size="md" />
+        <div class="header-font">Player Controls</div>
+      </div>
+      <!-- Menu Components -->
+      <HeaderMenu class="gt-xs" />
+      <HeaderMenuSmall class="lt-sm" />
+      <!-- <UserStatus /> -->
+    </q-header>
+
+    <q-page-container>
+      <div class="router-view">
+        <router-view />
+      </div>
+    </q-page-container>
+
+    <q-footer class="footer">
+      <!-- <PlayerControls /> -->
+    </q-footer>
+  </q-layout>
+</template>
+
+<style scoped>
+.center {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="harman logo" src="harman-logo.svg" style="width: 200px" />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
