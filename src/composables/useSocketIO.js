47b77b991179e25f3eb127660b0a0c81950d85e2
jkjh8 <jkjh82@naver.com>
2023-07-22 10:03:31 +0900

    socketio update event

--- src/composables/useSocketIO.js
+++ src/composables/useSocketIO.js
@@ -1,12 +1,21 @@
 import { io } from 'socket.io-client'
+import { pStatus } from './useStatus'
+import { pTime } from './useTime'
 
 let socket
 
 const initSocket = (address) => {
-  socket = io(address, {
+  socket = io(`${address}/ui`, {
     reconnectionDelayMax: 5000,
     transports: ['websocket'],
     autoConnect: true
+  })
+  socket.on('pStatus', (status) => {
+    pStatus.value = { ...status }
+    console.log('pStatus updated', pStatus.value)
+  })
+  socket.on('Ptime', (time) => {
+    pTime.value = { ...time }
   })
   socket.on('connect', () => {
     console.log(`connecting to socket.io id=${socket.id}`)
