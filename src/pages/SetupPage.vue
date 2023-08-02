696998d128b8746b8387b14c5f03c3be80e371aa
jkjh8 <jkjh82@naver.com>
2023-08-02 17:41:38 +0900

    update control page

--- src/pages/SetupPage.vue
+++ src/pages/SetupPage.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { onBeforeMount } from 'vue'
-
+import { pStatus } from 'src/composables/useStatus.js'
 // components
 import SetupHeader from 'src/components/setup/setupHeader'
 // import { playerState } from 'src/composables/usePlayerState'
@@ -22,7 +22,44 @@ import { api } from 'src/boot/axios'
       <q-card-section>
         <SetupHeader />
       </q-card-section>
-      <q-card-section>
+      <q-card-section class="q-px-lg q-gutter-y-sm">
+        <!-- device name -->
+        <div class="row no-wrap justify-between items-center">
+          <div class="text-weight-bold">NAME</div>
+          <div>{{ pStatus.name ? pStatus.name : 'No Name' }}</div>
+        </div>
+        <!-- device uuid -->
+        <div class="row no-wrap justify-between items-center">
+          <div class="text-weight-bold">Device ID</div>
+          <div class="row no-wrap items-center q-gutter-x-xs">
+            <div>
+              {{ pStatus.uuid ? pStatus.uuid : 'No ID' }}
+            </div>
+            <q-btn round flat color="primary" size="sm" icon="refresh"></q-btn>
+          </div>
+        </div>
+        <!-- device ip address list -->
+        <div class="row no-wrap justify-between items-start">
+          <div class="text-weight-bold">IP Address</div>
+          <div>
+            <div v-for="(item, idx) in pStatus.nics" :key="idx">
+              <div class="text-body text-blue-10">IPv4: {{ item.ipv4 }}</div>
+              <div class="text-caption">MAC: {{ item.mac }}</div>
+            </div>
+          </div>
+        </div>
+        <!-- device control port(web port) -->
+        <div class="row no-wrap justify-between items-center">
+          <div class="text-weight-bold">Control Port</div>
+          <div class="row no-wrap items-center">
+            <div>{{ pStatus.webport }}</div>
+            <q-btn round flat color="primary" size="sm" icon="refresh"></q-btn>
+          </div>
+        </div>
+        <div>audio device</div>
+        <div>show logo</div>
+        <div>update logo</div>
+        <div>start at fullscreen</div>
         <!-- <SelectAudioDevice />
         <SetupShowLogo />
         <SetupFullscreen /> -->
